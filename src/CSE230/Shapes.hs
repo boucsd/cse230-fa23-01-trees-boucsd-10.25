64ccbaffc09582e2dfad42a12fbe0741a8cbe88c
boucsd <126214698+boucsd@users.noreply.github.com>
2023-10-26 03:35:30 +0000

    shape 75%

--- src/CSE230/Shapes.hs
+++ src/CSE230/Shapes.hs
@@ -25,10 +25,24 @@ rainbow :: Image
 rainbow = foldr1 f xs
   where
     xs  = map g [1..7]
-    f   = error "fill this in"
-    g   = error "fill this in"
-
-
+    f   = overlay   --overlay
+    g x = circle (100 * x) solid (select_color x)
+      where 
+        select_color y
+          | y == 1  = violet
+          | y == 2  = blue
+          | y == 3  = cyan
+          | y == 4  =  green
+          | y == 5  =  yellow
+          | y == 6  =  orange
+          | otherwise = red
+      -- | x == 1  = circle 100 solid violet --draw circle with color
+      -- | x == 2  = circle 200 solid blue
+      -- | x == 3  = circle 300 solid cyan
+      -- | x == 4  = circle 400 solid green
+      -- | x == 5  = circle 500 solid yellow
+      -- | x == 6  = circle 600 solid orange
+      -- | otherwise = circle 700 solid red
 -------------------------------------------------------------------------------
 -- | ChessBoard with 'clone'
 -------------------------------------------------------------------------------
@@ -56,7 +70,7 @@ mkChess2   = save "img/chess2.png"   chessBoard2
 chessBoard2 :: Image
 chessBoard2 = iter 2 f base
   where
-    f       = error "fill this in"
+    f x     = aboves (clone 2 (besides (clone 2 x))) --error "fill this in"
     base    = gridSquare
 
 
@@ -71,7 +85,7 @@ sierpinskiTriangle1 = triRec 8
 
 triRec :: Int -> Image
 triRec 0 = blueTriangle
-triRec n = error "fill this in"
+triRec n = aboves [triRec (n - 1), besides [triRec (n - 1), triRec (n - 1)]] -- error "fill this in"
 
 blueTriangle :: Image
 blueTriangle = triangle 5 solid fgCol
@@ -85,7 +99,7 @@ mkTriangle2 = save "img/triangle2.png" sierpinskiTriangle2
 sierpinskiTriangle2 :: Image
 sierpinskiTriangle2 = iter 8 f base
  where
-   f               = error "fill this in"
+   f x             = aboves [x, besides [x, x]] -- error "fill this in"
    base            = blueTriangle
 
 
